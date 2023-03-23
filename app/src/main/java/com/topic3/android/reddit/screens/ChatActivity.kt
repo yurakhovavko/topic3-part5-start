8d0fb076bbfa1317f38375b3ca4c5e0276ce3a6b
Alexander <ufnfif@tut.by>
2023-03-23 14:04:55 +0300

    part5-start-v1

---
+++ app/src/main/java/com/topic3/android/reddit/screens/ChatActivity.kt
@@ -0,0 +1,31 @@
+package com.topic3.android.reddit.screens
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Button
+import com.topic3.android.reddit.databinding.ActivityChatBinding
+
+class ChatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChatBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+
+//        binding.button.setOnClickListener {
+//            showToast()
+//        }
+    }
+
+    private fun showToast() {
+        Toast.makeText(this, "Imaginary chat started!", Toast.LENGTH_SHORT).show()
+    }
+}
